3ec602215135989695a3e7adf28a0de8909033a0
Paris Klas <138509092+parisklas@users.noreply.github.com>
2023-09-26 17:11:07 +0000

    Completed assignment
    Uploaded the files manually; still struggling to connect vsc to git on mac.

---
+++ js/script.js
@@ -0,0 +1,159 @@
+// STEP 1 CAMELCASE
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+
+// STEP 2 EXAMPLES OF DATA TYPES
+/*
+757;                   // numeric literal
+"Green eggs and ham";  // string literal
+true;                  // boolean literal
+null;                  // null literal
+*/
+
+// STEP 3 EXAMPLES OF COMPLEX / VARIABLE EXPRESSIONS
+/*
+let anExpression = (33.8 − 32) × (5 / 9);
+let z = x * y;
+*/
+
+// STEP 4 DECLARE BUT DO NOT ASSIGN VARIABLES
+/*
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var iZipCode;
+var iYourAge;
+var strReferralSource;
+var bMayWeContactYou;
+*/
+
+// STEP 5 THREE WAYS OF ASSIGNING VARIABLES
+/*
+strFirstName = 'Paris';
+var iZipCode = 92107;
+var strReferralSource = '';
+*/
+
+// STEP 6 CREATE VARIABLE, ADD NUMBER AND STRING, DISPLAY COERCED RESULT
+/*
+var iYourAge = "Your age is " + 30;
+document.write(iYourAge);
+*/
+
+// STEP 7
+/*
+var bMayWeContactYou = "May We Contact You? " + false;
+console.log(bMayWeContactYou);
+var iYourAge = 30 + true;
+console.log(iYourAge);
+*/
+
+// STEP 8 STRING CORRECTION
+/*
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString);
+*/
+
+// STEP 9 NULL AND UNDEFINIED
+/*
+let varOne = null;
+console.log(varOne);
+let varTwo;
+console.log(varTwo);
+*/
+
+// STEP 10 TYPEOF OPERATOR
+/*
+let str = 'Hello World!'
+console.log(typeof str);      // returns string
+let n = 5;
+console.log(typeof n);        // returns number
+let here = true;
+console.log(typeof here);     // returns boolean
+const obj = {
+};
+console.log(typeof obj);      // returns object
+let grape;
+console.log(typeof grape);    // returns undefined
+*/
+
+// STEP 11 CONCATENATION
+/*
+window.alert("Hello " + "Paris Klas" + ", welcome to the JavaScript class!");
+*/
+
+// STEP 12
+/*
+let myName = "Paris Klas";
+window.alert("Hello " + myName + ", welcome to the JavaScript class!");
+*/
+
+// STEP 13
+
+// let myCourse = "JavaScript";
+// window.alert("Hello " + myName + ", welcome to the " + myCourse + " class!");
+
+
+// STEP 14
+// window.alert("Hello " + myName + ".\nWelcome to the " + myCourse + " class!");
+
+// STEP 15
+// let name = window.prompt('What is your name?');
+// window.alert("Hello " + name + ".\nWelcome to the " + myCourse + " class!");
+
+// STEP 16
+/*
+let name = window.prompt('What is your name?');
+let course = window.prompt('What class are you taking?');
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+// STEP 17
+/*
+let x = 10;
+let y = 20;
+console.log(x + y);
+*/
+
+// STEP 18
+/*
+let x = 20;
+console.log(x + 20);
+*/
+
+// STEP 19
+/*
+let x = 20;
+console.log(x * 5);
+*/
+
+// STEP 20
+/*
+let x = 20 % 3;
+console.log(x /= 1);
+*/
+
+// STEP 21
+/*
+let appTrue = true;
+if (appTrue) {
+  console.log(appTrue);
+}
+*/
+
+// STEP 22
+/*
+let appFalse = false;
+if (appFalse) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+*/
